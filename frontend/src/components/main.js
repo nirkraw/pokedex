0ea0cee4c625371b56b4599e4f5242b10c5c2e60
nirkraw <nirkaw@gmail.com>
2020-07-08 15:41:34 -0400

    add search functionality

--- frontend/src/components/main.js
+++ frontend/src/components/main.js
@@ -11,10 +11,14 @@ export default class Main extends React.Component {
             currentPokemon: {},
             pokeImg: "",
             pokeUrl: "",
-            pokemon: []
+            pokemon: [],
+            searchValue: "",
+            errorMsg: false
         }
         this.nextPokemon = this.nextPokemon.bind(this);
         this.previousPokemon = this.previousPokemon.bind(this);
+        this.update = this.update.bind(this);
+        this.search = this.search.bind(this);
     }
 
     componentDidMount() {
@@ -30,7 +34,8 @@ export default class Main extends React.Component {
         axios.get(this.state.pokeUrl).then(res => {
             const currentPokemon = res.data;
             const pokeImg = res.data.sprites.front_default;
-            this.setState({currentPokemon, pokeImg})
+            const currentPokeNum = currentPokemon.id - 1
+            this.setState({currentPokemon, pokeImg, currentPokeNum})
         })
     }
 
@@ -56,29 +61,65 @@ export default class Main extends React.Component {
         const pokeUrl = this.state.pokemon[currentPokeNum].url
         this.setState({currentPokeNum, pokeUrl});
     }
+
+    update() {
+         return (e) => {
+           this.setState({
+             searchValue: e.currentTarget.value,
+           });
+         };
+    }
     
     search() {
-        console.log(this.state.pokemon);
+        let name = this.state.searchValue.toLowerCase();
+        for (let i = 0; i < this.state.pokemon.length; i++) {
+            const currentPoke = this.state.pokemon[i];
+            if(currentPoke.name === name) {
+                this.setState({pokeUrl: currentPoke.url, searchValue: ""})
+                return null;
+            }
+        }
+       this.errorMessage();
+    }
+
+    errorMessage() {
+        this.setState({errorMsg: true});
+        setTimeout(() => {
+              this.setState({ errorMsg: false });
+        }, 3000);
     }
 
     render() {
-        const {pokeImg, currentPokemon} = this.state;
-   
+        const {pokeImg, currentPokemon, errorMsg} = this.state;
 
         return (
           <div className="secondary-container">
             <div className="third-container">
+                  <div className="search-container">
+                      <input 
+                      placeholder="Search..."
+                      value={this.state.searchValue}
+                      onChange={this.update()}
+                      />
+                      <button onClick={this.search}>Go!</button>
+                  </div>
               <div className="image-parent-container">
                 <div className="image-container">
                   <img className="image" src={pokeImg} alt="pokemon"></img>
                 </div>
               </div>
+              {!errorMsg ?
               <div className="poke-stats-container">
                 <h3 className="number">#{currentPokemon.id}</h3>
                 <p className="name">Name: {currentPokemon.name}</p>
                 <p className="height">Height: {currentPokemon.height}</p>
                 <p className="weight">Weight: {currentPokemon.weight}</p>
               </div>
+              :
+               <div className="poke-stats-container">
+                <h1 className="error">Could not find any pokemon by that name</h1>
+               </div>
+                }
               <div className="arrow-container">
                 <img className="left-arrow" src={arrow} alt="arrow" onClick={this.previousPokemon}></img>
                 <img
